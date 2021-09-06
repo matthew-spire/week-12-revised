cf722aa4700c88d509735bf1f2daef4cd8ab4696
Matthew Spire <matthew_spire@yahoo.com>
2021-09-06 08:54:20 -0700

    added some additional code to reduce learner confusion

--- es6React/getData0.jsx
+++ es6React/getData0.jsx
@@ -1,6 +1,13 @@
-// App component that will be rendered into the root div of standalone.html
 // Add and remove code as necessary to follow along with Dr. Williams
+
+// const name = "Mart";
+// const element = <h1>Hello {name}</h1>
+// ReactDOM.render(<App />, document.getElementById("root"));
+
+// App component that will be rendered into the root div of standalone.html
 const App = () => {
+  const name = "Fred";
+  return (<h1>Hello {name}</h1>)
 }
 //---------------
 ReactDOM.render(<App />, document.getElementById("root"));
