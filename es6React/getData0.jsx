981c21d90b1054fdc75237db36838e02f673a2ae
Matthew Spire <matthew_spire@yahoo.com>
2021-09-06 08:33:25 -0700

    first commit

---
+++ es6React/getData0.jsx
@@ -0,0 +1,6 @@
+// App component that will be rendered into the root div of standalone.html
+// Add and remove code as necessary to follow along with Dr. Williams
+const App = () => {
+}
+//---------------
+ReactDOM.render(<App />, document.getElementById("root"));
